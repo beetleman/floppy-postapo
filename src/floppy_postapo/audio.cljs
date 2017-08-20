50a79cdba355ff80e7cb566dfbf802791f728bf8
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-08-20 17:09:13 +0200

    fix audio

--- src/floppy_postapo/audio.cljs
+++ src/floppy_postapo/audio.cljs
@@ -2,10 +2,10 @@
   (:require [mount.core :refer [defstate]]))
 
 
-(defstate main :start (let [audio (js/Audio. "/audio/main.ogg")]
+(defstate main :start (let [audio (js/Audio. "audio/main.ogg")]
                         (set! (.-loop audio) js/true)
                         audio))
 
-(defstate splash :start (let [audio (js/Audio. "/audio/splash.ogg")]
+(defstate splash :start (let [audio (js/Audio. "audio/splash.ogg")]
                         (set! (.-loop audio) js/true)
                         audio))
